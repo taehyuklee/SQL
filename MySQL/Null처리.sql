5da5d490292004e1e25d1e936c170be2530a0687
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-10-21 00:24:35 +0900

    Update Null처리.sql

--- MySQL/Null처리.sql
+++ MySQL/Null처리.sql
@@ -1,4 +1,4 @@
-SELECT IF(NULL(PT_NO, "NONE")) AS PT_NO, PT_NAME, GEND_CD, AGE, TLNO FROM PATIENT
+SELECT PT_NO, PT_NAME, GEND_CD, AGE, IFNULL(TLNO, "NONE") AS TLNO FROM PATIENT
 
 # IFNULL(TLNO, "NONE") #해석 -> 만약 PT_NO 컬럼이 NULL이라면 -> "NONE"으로 채워 넣는다.
 #그리고 만약 AS를 통해 Alias를 정해주지 않는다면, IF(NULL(PT_NO, "NONE") 자체가 컬럼에 들어가게 된다.
